0b9328b29d6ad30ab8a4da904c7ca1e2c4083cbf
Siddh8051 <siddharthkumar805100@gmail.com>
2024-10-11 12:35:31 +0530

    Update get_func.py

--- devgagan/core/get_func.py
+++ devgagan/core/get_func.py
@@ -438,7 +438,7 @@ async def set_rename_command(user_id, custom_rename_tag):
 # Function to get the user's custom renaming preference
 def get_user_rename_preference(user_id):
     # Retrieve the user's custom renaming tag if set, or default to 'Team SPY'
-    return user_rename_preferences.get(str(user_id), 'Team SPY')
+    return user_rename_preferences.get(str(user_id), 'Team')
 
 # Function to set custom caption preference
 async def set_caption_command(user_id, custom_caption):
@@ -465,7 +465,7 @@ async def settings_command(event):
         [Button.inline("Remove Words", b'delete'), Button.inline("Reset", b'reset')],
         [Button.inline("Login", b'addsession'), Button.inline("Logout", b'logout')],
         [Button.inline("Set Thumbnail", b'setthumb'), Button.inline("Remove Thumbnail", b'remthumb')],
-        [Button.url("Report Errors", "https://t.me/devgaganin")]
+        [Button.url("Report Errors", "https://t.me/durov")]
     ]
     
     await gf.send_file(
